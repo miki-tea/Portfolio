3f00badc90dee615927f3e734916bca70cd624cb
miki <miki.ishii16@gmail.com>
2020-03-20 03:58:23 +0900

    slickをレスポンシブ 対応にしました。

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
-import jQuery from 'jquery';
-import 'slick-carousel';
+import jQuery from "jquery";
+import "slick-carousel";
 
 const $ = jQuery;
-console.log('read jQuery File!');
-$(function () {
-  $('.slider').slick({
+console.log("read jQuery File!");
+$(function() {
+  $(".slider").slick({
     autoplay: true,
-    autoplaySpeed: 1500
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true
   });
 });
-
