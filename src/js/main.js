c458e3054bb4d8e5b44081675e93d88b560c13fb
miki <miki.ishii16@gmail.com>
2020-03-25 23:00:16 +0900

    ハンバーガーメニュー実装

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,16 @@ import "slick-carousel";
 
 const $ = jQuery;
 console.log("read jQuery File!");
+
 $(function() {
   $(".slider").slick({
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
     dots: true
+  });
+
+  $(".header__toggle").on("click", function() {
+    $(".header__toggle, .nav, .toggle__icon, .nav__bg").toggleClass("show");
   });
 });
