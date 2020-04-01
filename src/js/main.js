4244770510b1eb9092cb39b32b2ffebc121e5d04
miki <miki.ishii16@gmail.com>
2020-04-01 23:29:50 +0900

    スクロールトップボタン実装

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,27 @@ $(function() {
   $(".header__toggle, .nav__cover").on("click", function() {
     $(".header__toggle, .nav, .toggle__icon, .nav__cover").toggleClass("show");
   });
+
+  // 少しスクロールするとトップへスクロールするボタンが現れる
+  var scrollBtn = $("#js-pageReverse");
+  // scrollBtn.hide();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      scrollBtn.addClass("js-scrollIn");
+    } else {
+      scrollBtn.removeClass("js-scrollIn");
+    }
+  });
+  // ボタンを押すとトップへスクロール
+  scrollBtn.click(function() {
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      300,
+      "linear"
+    );
+    return false;
+  });
 });
